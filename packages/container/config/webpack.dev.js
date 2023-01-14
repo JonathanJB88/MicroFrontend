4dc84c1fb2b899eba179954f865b2b5e8232a24e
Jonathan Bracho <jonathandev@macbook-pro-de-jonathan.home>
2023-01-14 16:53:14 +0100

    Created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,7 +1,6 @@
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const packageJson = require('../package.json');
 
 const devConfig = {
@@ -20,9 +19,6 @@ const devConfig = {
         marketing: 'marketing@http://localhost:8081/remoteEntry.js',
       },
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
     }),
   ],
 };
