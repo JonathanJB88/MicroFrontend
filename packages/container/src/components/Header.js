53793c1583c6db9b416dde93f1735482536390bd
Jonathan Bracho <jonathandev@macbook-pro-de-jonathan.home>
2023-01-17 14:32:21 +0100

    added dashboard  fixed

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ isSignedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            App
+            MicroFrontend App
           </Typography>
           <Button
             color="primary"
